e82f6c9f61024a8c2d5f5573505d250ce61a6092
Bruno Maciel <brunosazanamaciel@gmail.com>
2022-09-05 14:56:34 -0300

    Removido algumas coisas fora do lugar

--- src/containers/Post/index.tsx
+++ src/containers/Post/index.tsx
@@ -3,7 +3,7 @@ import 'tailwindcss/tailwind.css';
 
 import Header from '../../components/Header';
 import { PostData } from '../../domain/posts/post';
-import { AuthorContainer, Container } from './styles';
+import { Container } from './styles';
 
 export type SinglePostProps = {
   post: PostData;
@@ -13,13 +13,13 @@ export default function PostPage({ post }: SinglePostProps) {
   return (
     <>
       <Header />
-      <h1>{post.attributes.createdAt}</h1>
+      {/* <h1>{post.attributes.createdAt}</h1> */}
       <Container
         dangerouslySetInnerHTML={{ __html: post.attributes.content }}
       ></Container>
-      <AuthorContainer className="bg-red-800">
+      {/* <AuthorContainer className="bg-red-800">
         {post.attributes.author.data.attributes.name}
-      </AuthorContainer>
+      </AuthorContainer> */}
       <Footer />
     </>
   );
